f0ac1e4e9c7f7577bed5bb9259f1607f508af94e
Xu Zhang <xuzhang@pomelo.cs.columbia.edu>
2016-09-22 15:45:07 -0400

    feature good

---
+++ tools/feature_extractor.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Faster R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""
+Demo script showing detections in sample images.
+
+See README.md for installation instructions before running.
+"""
+
+import _init_paths
+from fast_rcnn.config import cfg
+from fast_rcnn.feature_extract import im_feature_extract
+from fast_rcnn.nms_wrapper import nms
+from utils.timer import Timer
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import caffe, os, sys, cv2
+import argparse
+
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+NETS = {'vgg16': ('VGG16',
+                  'VGG16_faster_rcnn_final.caffemodel'),
+        'zf': ('ZF',
+                  'ZF_faster_rcnn_final.caffemodel')}
+
+def demo(net, image_name):
+    """Detect object classes in an image using pre-computed object proposals."""
+
+    # Load the demo image
+    im_file = os.path.join(cfg.WORK_DIR+cfg.DATASET,cfg.SUB_NAME,image_name)
+    im = cv2.imread(im_file)
+    #im = cv2.flip(im,1)
+    # Detect all object classes and regress object bounds
+    timer = Timer()
+    timer.tic()
+    features, boxes = im_feature_extract(net, im)
+    save_object = np.zeros((2,),dtype=np.object)
+    save_object[0] = features
+    save_object[1] = boxes
+    sio.savemat(cfg.WORK_DIR+cfg.DATASET+'/'+cfg.SUB_NAME+'_frcnn_pool3/' + \
+            image_name[0:-3]+'mat',{'save_object':save_object})
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Faster R-CNN demo')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+                        default=0, type=int)
+    parser.add_argument('--cpu', dest='cpu_mode',
+                        help='Use CPU mode (overrides --gpu)',
+                        action='store_true')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
+                        choices=NETS.keys(), default='vgg16')
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+    cfg.WORK_DIR = '/home/xuzhang/project/Medifor/data/'
+    cfg.DATASET = 'CASIA2'
+    cfg.SUB_NAME = 'images'
+    args = parse_args()
+
+    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
+                            'faster_rcnn_alt_opt', 'extract_rcnn_test.pt')
+    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
+                              NETS[args.demo_net][1])
+
+    if not os.path.isfile(caffemodel):
+        raise IOError(('{:s} not found.\nDid you run ./data/script/'
+                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+
+    if args.cpu_mode:
+        caffe.set_mode_cpu()
+    else:
+        caffe.set_mode_gpu()
+        caffe.set_device(args.gpu_id)
+        cfg.GPU_ID = args.gpu_id
+    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+    print '\n\nLoaded network {:s}'.format(caffemodel)
+
+    os.system('mkdir -p '+cfg.WORK_DIR+cfg.DATASET+'/'+cfg.SUB_NAME+'_frcnn_pool3/');
+    
+    for im_name in os.listdir(cfg.WORK_DIR+cfg.DATASET+'/'+cfg.SUB_NAME):
+        if im_name.endswith(".jpg") or im_name.endswith(".JPG") or \
+            im_name.endswith(".png") or im_name.endswith(".PNG") or \
+            im_name.endswith(".tif") or im_name.endswith(".TIF") or \
+            im_name.endswith(".bmp") or im_name.endswith(".BMP"):
+
+            print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+            print 'Extract feature for {}'.format(im_name)
+            demo(net, im_name)
+
+    plt.show()
